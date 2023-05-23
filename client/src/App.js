5b3cb79a24fe45c6be0890739383fd9780e64750
Mhorvilleur13 <mhorvilleur13@gmail.com>
2023-05-23 11:08:19 -0600

    second vid done

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Axios from "axios";
 function App() {
+  const [movieName, setMovieName] = useState("");
+  const [review, setReview] = useState("");
+  const [movieReviewList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/api/get").then((response) => {
+      setMovieList(response.data);
+    });
+  }, []);
+
+  const submitReview = () => {
+    Axios.post("http://localhost:3001/api/insert", {
+      movieName: movieName,
+      movie_reviewscol: review,
+    }).then(() => {
+      alert("success");
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CRUD Application</h1>
+      <div className="form">
+        <label>Movie Name:</label>
+        <input
+          type="text"
+          name="movieName"
+          onChange={(e) => {
+            setMovieName(e.target.value);
+          }}
+        ></input>
+        <label>Review: </label>
+        <input
+          type="text"
+          name="review"
+          onChange={(e) => {
+            setReview(e.target.value);
+          }}
+        ></input>
+        <button onClick={submitReview}>Submit</button>
+        {movieReviewList.map((val) => {
+          return (
+            <h1>
+              Movie Name: {val.movieName} | Movie Review: {val.movie_reviewscol}
+            </h1>
+          );
+        })}
+      </div>
     </div>
   );
 }
